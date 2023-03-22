aeafe337c3cf89bdb7f247e91e170eed6b33339e
prachi-tote <127733520+prachi-tote@users.noreply.github.com>
2023-03-23 01:53:27 +0530

    Add files via upload
    Calculate Daily
    Employee Wage

---
+++ UC-2.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+isPresent=1;
+randomCheck=$((RANDOM%2))
+if [ $isPresent -eq $randomCheck ];
+then
+	empWagePerHrs=20
+	empHrs=8
+	salary=$(( $empHrs*$empWagePerHrs ))
+
+echo	"Employee is present";
+echo	"$salary"
+else
+        salary=0;
+echo	"Employee is absent";
+fi
+
